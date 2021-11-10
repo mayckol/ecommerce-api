d11578a3fa64cabe42ceba322015ae940f7db8d8
Mayckol Ferreira <mayckol.ferreira@basis.com.br>
2021-11-10 14:17:34 -0300

    add sanctum to control auth

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/', function () {
+    $users = \App\Models\User::all();
     return response()->json([
-        'foo' => 'bar',
+        'foo' => $users,
     ]);
 });
