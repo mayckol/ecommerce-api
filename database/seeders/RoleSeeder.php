4603c5c661b377f39d654505ed3a7f5095cac3df
Mayckol Ferreira <mayckol.ferreira@basis.com.br>
2021-11-10 16:57:42 -0300

    add seeders

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->truncate();
+        DB::table('roles')->insert([
+            'id' => config('role.adm'),
+            'type' => 'ADM',
+        ]);
+        DB::table('roles')->insert([
+            'id' => config('role.client'),
+            'type' => 'CLIENT',
+        ]);
+    }
+}
