7a3d833aaa6c1898395680e8077852dee9d800df
Mayckol Ferreira <mayckol.ferreira@basis.com.br>
2021-11-21 19:27:29 -0300

    modifica o tipo de auth para mobile usando token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,34 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Contracts\Auth\Authenticatable;
+use App\Repositories\UserRepository;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 
 class AuthController extends Controller
 {
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * Handle an authentication attempt.
      *
+     * @throws ValidationException
      */
     public function authenticate(Request $request)
     {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return auth()->user();
+        $user = $this->userRepository->findWhere(['email' => $request->get('email')])->first();
+        if (!$user || !Hash::check($request->get('password'), $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
         }
 
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
+        return $user->createToken('access_token')->plainTextToken;
+        /* Using Session */
+//        $credentials = $request->validate([
+//            'email' => ['required', 'email'],
+//            'password' => ['required'],
+//        ]);
+//        if (Auth::attempt($credentials)) {
+//            $request->session()->regenerate();
+//            return auth()->user();
+//        }
+//
+//        return back()->withErrors([
+//            'email' => 'The provided credentials do not match our records.',
+//        ]);
     }
 
     /**
